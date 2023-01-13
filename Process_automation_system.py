05df5f1c5fe589db9ca97b8630ae52a88e0076e8
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-01-12 23:55:18 -0300

    Update Process_automation_system.py

--- Process_automation_system.py
+++ Process_automation_system.py
@@ -1,50 +1,59 @@
-# automaton process system
+# automation process system
 import requests;
 import json;
 
 # Para conseguir controlar o mouse, teclado e tela do computador, eh necessario instalar a biblioteca abaixo:
-import pyautogui
+import pyautogui as pag;
 import pyperclip;
 import time;
+import pandas as pd;
 
+pag.PAUSE = 1;
+pag.press
 
-pyautogui.PAUSE = 1;
-pyautogui.press
-
-pyautogui.hotkey("alt","TAB");
-pyautogui.hotkey("ctrl","t");
+pag.hotkey("alt","TAB");
+pag.hotkey("ctrl","t");
 pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga")
-pyautogui.hotkey("ctrl","v");
-pyautogui.press("enter");
+pag.hotkey("ctrl","v");
+pag.press("enter");
 
 
 time.sleep(5);
-pyautogui.click(x=378, y=281, clicks=3); # ENTRAR NA PASTA DO RELATORIO DE VENDAS
+pag.click(x=378, y=281, clicks=3); # ENTRAR NA PASTA DO RELATORIO DE VENDAS
 
 time.sleep(5);
-pyautogui.click(x=355, y=357, clicks=1); # SELECIONAR ARQUIVO
+pag.click(x=355, y=357, clicks=1); # SELECIONAR ARQUIVO
 
-pyautogui.click(x=1116, y=191, clicks=1); # CLICANDO NO "3 pontinhos"
+pag.click(x=1116, y=191, clicks=1); # CLICANDO NO "3 pontinhos"
 
 time.sleep(1);
-pyautogui.click(x=917, y=587, clicks=1); # CLICANDO EM DOWNLOAD
+pag.click(x=917, y=587, clicks=1); # CLICANDO EM DOWNLOAD
+
+tabela = pd.read_excel(r"C:\Users\Patrick\Downloads\Vendas - Dez.xlsx")
+
+print(tabela);
+
+faturamento = tabela["Valor Final"].sum();
+quantidade = tabela["Quantidade"].sum();
 
 
+
+
+
+
+
+
+"""
 # print("Terminado.....");
 # time.sleep(5);
 print(pyautogui.position());
 
 
-
-"""
-pyautogui.click();
-pyautogui.write();
-pyautogui.press();
-"""
+# CALCULAR OS INDICADORES (FATURAMENTO E QUANTIDADE DE PRODUTOS)
 
 
 
-"""
+
 response = requests.get("https://pokeapi.co/api/v2/pokemon/pikachu");
 
 class Pokemon:
