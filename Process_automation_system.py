0fd4e9a7d4b0a87fd504cb5b0234e3ac6cb0a4d0
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-01-13 23:36:45 -0300

    Update Process_automation_system.py

--- Process_automation_system.py
+++ Process_automation_system.py
@@ -10,7 +10,7 @@ import pandas as pd;
 
 pag.PAUSE = 1;
 pag.press
-
+"""
 pag.hotkey("alt","TAB");
 pag.hotkey("ctrl","t");
 pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga")
@@ -28,17 +28,27 @@ pag.click(x=1116, y=191, clicks=1); # CLICANDO NO "3 pontinhos"
 
 time.sleep(1);
 pag.click(x=917, y=587, clicks=1); # CLICANDO EM DOWNLOAD
+"""
+
 
 tabela = pd.read_excel(r"C:\Users\Patrick\Downloads\Vendas - Dez.xlsx")
-
-print(tabela);
 
 faturamento = tabela["Valor Final"].sum();
 quantidade = tabela["Quantidade"].sum();
 
+print("Soma do faturamento: ",faturamento);
+print("Soma da quantitade: ",quantidade);
 
+pag.hotkey("alt","TAB");
+pag.hotkey("ctrl","t");
+pag.write("https://mail.google.com/mail/u/0/#inbox");
+pag.press("enter");
 
-
+time.sleep(5);
+pag.click(x=98, y=200, clicks=1);
+time.sleep(1);
+pag.write("marcospatrick039474@gmail.com");
+pag.press("tab");
 
 
 
