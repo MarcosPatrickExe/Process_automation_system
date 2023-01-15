e20ff3cb07f5af884225cdf97c9fbc42e861052c
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-01-15 18:30:57 -0300

    Add files via upload

---
+++ web_scrapping/web_automation _system.py
@@ -0,0 +1,16 @@
+from selenium import webdriver;
+
+browser = webdriver.Edge();
+
+browser.get("https://www.google.com/");
+
+
+
+
+## OBTENDO AS COTACOES DO DOLAR, EURO E OURO DA WEB:
+
+
+# PARA QUE O SELENIUM FUNCIONE, EH NECESSARIO BAIXAR O DRIVER QUE ESTEJA RELACIONADO A VERSAO DO SEU BROWSER E COLOCA-LO NA PASTA DE INSTALACAO DO PYTHON
+# PARA DESCOBRIR LOCALIZACAO DO PYTHON, EXECUTE:
+# python -c "import sys; print(sys.executable)"
+
